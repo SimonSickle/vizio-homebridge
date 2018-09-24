fe45c024f02a7a52fc4569adab5ea6ba7b87a395
Simon Sickle <simon@simonsickle.com>
2018-09-23 22:40:47 -0500

    Update for volume control
    Signed-off-by: Simon Sickle <simon@simonsickle.com>

--- index.js
+++ index.js
@@ -13,12 +13,12 @@ function VizioSwitch(log, config) {
   this.name = config["name"];
   this.authToken = config["authToken"];
   this.ipAddress = config["ipAddress"];
+  this.model = ""
+  this.mfg = ""
+
   if (typeof this.configIpAddress === "undefined") {
     this.discover();
   }
-
-  // TODO: validate config before using it
-  // TODO: find the IP from name discovery if it isn't specified
 
   this.tv = new smartcast(this.ipAddress, this.authToken);
 
@@ -26,35 +26,37 @@ function VizioSwitch(log, config) {
 }
 
 VizioSwitch.prototype.getPowerState = function (callback) {
-  this.log("TODO: getPowerState");
   if (typeof this.configIpAddress === "undefined") {
     this.discover();
   }
 
   this.tv.power.currentMode().then((data) => {
-    this.log("tv.power.currentMode() = ", data);
     callback(null, (data.ITEMS[0].VALUE == 1));
   });
 }
 
 VizioSwitch.prototype.getMuteState = function (callback) {
-  this.log("TODO: getMuteState");
   if (typeof this.configIpAddress === "undefined") {
     this.discover();
   }
 
   this.tv.control.volume.getMuteState().then((data) => {
-    this.log("tv.controle.volume.getMuteState() = ", data);
     callback(null, data.ITEMS[0].ENABLED == "TRUE");
   });
 }
 
-VizioSwitch.prototype.setMuteState = function (mute, callback) {
-  this.log("TODO: setMuteState");
+// Used for volume "power" right now
+VizioSwitch.prototype.alwaysTrueState = function (callback) {
   if (typeof this.configIpAddress === "undefined") {
     this.discover();
   }
+  callback(null, true)
+}
 
+VizioSwitch.prototype.setMuteState = function (mute, callback) {
+  if (typeof this.configIpAddress === "undefined") {
+    this.discover();
+  }
 
   if (mute) {
     this.tv.control.volume.mute();
@@ -66,31 +68,26 @@ VizioSwitch.prototype.setMuteState = function (mute, callback) {
 }
 
 VizioSwitch.prototype.getVolumeState = function (callback) {
-  this.log("TODO: getVolumeState");
   if (typeof this.configIpAddress === "undefined") {
     this.discover();
   }
 
   this.tv.control.volume.get().then((data) => {
-    this.log("tv.controle.volume.get() = ", data);
     callback(null, data.ITEMS[0].VALUE);
   });
 }
 
 VizioSwitch.prototype.setVolumeState = function (volume, callback) {
-  this.log("TODO: setVolumeState");
   if (typeof this.configIpAddress === "undefined") {
     this.discover();
   }
 
   this.tv.control.volume.set(volume).then((data) => {
-    this.log("tv.controle.volume.set() = ", data);
     callback(null, (data.ITEMS[0].VALUE == volume));
   });
 }
 
 VizioSwitch.prototype.setPowerState = function (powerOn, callback) {
-  this.log("TODO: setPowerState ", powerOn);
   if (typeof this.configIpAddress === "undefined") {
     this.discover();
   }
@@ -103,59 +100,48 @@ VizioSwitch.prototype.setPowerState = function (powerOn, callback) {
   callback(); // success
 }
 
-VizioSwitch.prototype.identify = function (callback) {
-  this.log("identify");
-
-  if (typeof this.configIpAddress === "undefined") {
-    this.discover();
-  }
-
-  this.tv.control.volume.toggleMute();
-  callback(); // success
-}
-
 VizioSwitch.prototype.discover = function () {
-  // TODO: how do we handle more than one device found.
   smartcast.discover((device) => {
-    if (this.name.localeCompare(device.name) == 0) {
-      this.log("TODO: discover found device: ", device);
+    if (this.ipAddress.localeCompare(device.ip) == 0) {
       this.tv = new smartcast(device.ip, this.authToken);
       this.model = device.model;
+      this.mfg = device.manufacturer
     } else {
-      this.log("TODO: discover wrong name", device.name, "not", this.name);
+      this.log("TODO: did not match config IP")
     }
   });
-  
-  // Example output:
-  // {
-  //     ip: '192.168.0.131',
-  //     name: 'Living Room',
-  //     manufacturer: 'VIZIO',
-  //     model: 'P65-C1'
-  // }
 }
 
 VizioSwitch.prototype.getServices = function () {
   var informationService = new Service.AccessoryInformation();
-
   informationService
-    .setCharacteristic(Characteristic.Manufacturer, "Vizio")
-    .setCharacteristic(Characteristic.Model, this.model)
-    .setCharacteristic(Characteristic.SerialNumber, "TODO Serial Number");
+      .updateCharacteristic(Characteristic.Manufacturer, this.mfg)
+      .updateCharacteristic(Characteristic.Model, this.model);
 
-  var speakerService = new Service.Speaker(this.name);
-  speakerService
-                .addCharacteristic(new Characteristic.On())
+  // Power is just a switch, not depending on volume
+  var powerService = new Service.Switch(this.name + " power", "power")
+  powerService
+  .getCharacteristic(Characteristic.On)
                 .on("get", this.getPowerState.bind(this))
                 .on("set", this.setPowerState.bind(this));
 
-  speakerService.addCharacteristic(new Characteristic.Volume())
-            .on("get", this.getVolumeState.bind(this))
-            .on("set", this.setVolumeState.bind(this));
+  // Volume is a lightbulb since homekit doesnt properly support speakers
+  var volumeService = new Service.Lightbulb(this.name + " volume");
+  volumeService
+                .addCharacteristic(new Characteristic.Brightness())
+                .on('get', this.getVolumeState.bind(this))
+                .on('set', this.setVolumeState.bind(this));
+  // Without this always being "on" the device will not show volume percent
+  volumeService
+                .getCharacteristic(Characteristic.On)
+                .on("get", this.alwaysTrueState.bind(this));
 
-  speakerService.getCharacteristic(Characteristic.Mute)
-  .on("get", this.getMuteState.bind(this))
-  .on("set", this.setMuteState.bind(this));
+  // Mute is a switch since homekit doesnt properly support speakers
+  var muteService = new Service.Switch(this.name + " speakers", "mute")
+  muteService
+                .getCharacteristic(Characteristic.On)
+                .on("get", this.getMuteState.bind(this))
+                .on("set", this.setMuteState.bind(this));
 
-  return [informationService, speakerService];
+  return [informationService, powerService, volumeService, muteService];
 }
